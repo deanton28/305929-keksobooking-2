9cc29afd44eda5ad1d76a615d29ea46cd2368f85
Anton Dev <deanton28@gmail.com>
2018-04-23 00:54:09 +0300

    add backend.js

---
+++ js/backend.js
@@ -0,0 +1,12 @@
+'use strict';
+
+window.backend = {};
+
+(function (exports) {
+  var URLLOAD = 'https://js.dump.academy/keksobooking/data';
+  var URLSAVE = 'https://js.dump.academy/keksobooking';
+
+  exports.load = function () {
+
+  };
+})(window.backend);
